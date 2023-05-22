b586cb370e11a63586ab017587ff0e27b88bc330
Vincent Desmouceaux <desmontvincent@gmail.com>
2023-05-22 19:00:12 +0200

    préparation route event

---
+++ routes/event.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const fileUpload = require("express-fileupload");
+const convertToBase64 = require("../utils/convertToBase64");
+const cloudinary = require("cloudinary").v2;
+
+const User = require("../models/User");
+const Promoter = require("../models/Promoter");
+const Ticket = require("../models/Ticket");
+const Event = require("../models/Event");
+
+const isAuthenticated = require("../middlewares/promoterAuthenticated");
+
+module.exports = router;
